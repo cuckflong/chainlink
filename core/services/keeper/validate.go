d6b077428c046570b57b34e2c7a213c012c2197f
Vlad <vladgh18@gmail.com>
2022-05-11 15:31:23 +0300

    Fixed formatting issue caused by the observation source string

--- core/services/keeper/validate.go
+++ core/services/keeper/validate.go
@@ -10,31 +10,31 @@ import (
 
 const ExpectedObservationSource = `
 	encode_check_upkeep_tx   [type=ethabiencode
-							  abi="checkUpkeep(uint256 id, address from)"
-							  data="{\"id\":$(jobSpec.upkeepID),\"from\":$(jobSpec.fromAddress)}"]
+                              abi="checkUpkeep(uint256 id, address from)"
+                              data="{\"id\":$(jobSpec.upkeepID),\"from\":$(jobSpec.fromAddress)}"]
 	check_upkeep_tx          [type=ethcall
-							  failEarly=true
-							  extractRevertReason=true
-							  evmChainID="$(jobSpec.evmChainID)"
-							  contract="$(jobSpec.contractAddress)"
-							  gas="$(jobSpec.checkUpkeepGasLimit)"
-							  gasPrice="$(jobSpec.gasPrice)"
-							  gasTipCap="$(jobSpec.gasTipCap)"
-							  gasFeeCap="$(jobSpec.gasFeeCap)"
-							  data="$(encode_check_upkeep_tx)"]
+                              failEarly=true
+                              extractRevertReason=true
+                              evmChainID="$(jobSpec.evmChainID)"
+                              contract="$(jobSpec.contractAddress)"
+                              gas="$(jobSpec.checkUpkeepGasLimit)"
+                              gasPrice="$(jobSpec.gasPrice)"
+                              gasTipCap="$(jobSpec.gasTipCap)"
+                              gasFeeCap="$(jobSpec.gasFeeCap)"
+                              data="$(encode_check_upkeep_tx)"]
 	decode_check_upkeep_tx   [type=ethabidecode
-							  abi="bytes memory performData, uint256 maxLinkPayment, uint256 gasLimit, uint256 adjustedGasWei, uint256 linkEth"]
+                              abi="bytes memory performData, uint256 maxLinkPayment, uint256 gasLimit, uint256 adjustedGasWei, uint256 linkEth"]
 	encode_perform_upkeep_tx [type=ethabiencode
-							  abi="performUpkeep(uint256 id, bytes calldata performData)"
-							  data="{\"id\": $(jobSpec.upkeepID),\"performData\":$(decode_check_upkeep_tx.performData)}"]
+                              abi="performUpkeep(uint256 id, bytes calldata performData)"
+                              data="{\"id\": $(jobSpec.upkeepID),\"performData\":$(decode_check_upkeep_tx.performData)}"]
 	perform_upkeep_tx        [type=ethtx
-							  minConfirmations=0
-							  to="$(jobSpec.contractAddress)"
-							  from="[$(jobSpec.fromAddress)]"
-							  evmChainID="$(jobSpec.evmChainID)"
-							  data="$(encode_perform_upkeep_tx)"
-							  gasLimit="$(jobSpec.performUpkeepGasLimit)"
-							  txMeta="{\"jobID\":$(jobSpec.jobID),\"upkeepID\":$(jobSpec.prettyID)}"]
+                              minConfirmations=0
+                              to="$(jobSpec.contractAddress)"
+                              from="[$(jobSpec.fromAddress)]"
+                              evmChainID="$(jobSpec.evmChainID)"
+                              data="$(encode_perform_upkeep_tx)"
+                              gasLimit="$(jobSpec.performUpkeepGasLimit)"
+                              txMeta="{\"jobID\":$(jobSpec.jobID),\"upkeepID\":$(jobSpec.prettyID)}"]
 	encode_check_upkeep_tx -> check_upkeep_tx -> decode_check_upkeep_tx -> encode_perform_upkeep_tx -> perform_upkeep_tx
 `
 
