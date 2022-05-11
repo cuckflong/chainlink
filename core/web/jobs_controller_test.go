816d1e0d55c492fe9141f9166bfa30b10712b64b
Vlad <vladgh18@gmail.com>
2022-05-11 18:04:28 +0300

    Replaced the test observation source with empty string

--- core/web/jobs_controller_test.go
+++ core/web/jobs_controller_test.go
@@ -180,9 +180,10 @@ func TestJobController_Create_HappyPath(t *testing.T) {
 		{
 			name: "keeper",
 			toml: testspecs.GenerateKeeperSpec(testspecs.KeeperSpecParams{
-				Name:            "example keeper spec",
-				ContractAddress: "0x9E40733cC9df84636505f4e6Db28DCa0dC5D1bba",
-				FromAddress:     "0xa8037A20989AFcBC51798de9762b351D63ff462e",
+				Name:              "example keeper spec",
+				ContractAddress:   "0x9E40733cC9df84636505f4e6Db28DCa0dC5D1bba",
+				FromAddress:       "0xa8037A20989AFcBC51798de9762b351D63ff462e",
+				ObservationSource: "",
 			}).Toml(),
 			assertion: func(t *testing.T, r *http.Response) {
 				require.Equal(t, http.StatusOK, r.StatusCode)
